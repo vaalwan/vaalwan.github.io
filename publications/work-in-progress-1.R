50149df967ba604ca5508faf8651b4be38140c89
vaalwan <valwan@chicagobooth.edu>
2025-04-14 14:09:21 -0500

    Initial commit

---
+++ publications/work-in-progress-1.R
@@ -0,0 +1,11 @@
+---
+  title: "Cross Product Learning with Product Label Data"
+authors:
+  - name: "Vanessa A. Alwan"
+affiliation: "The University of Chicago Booth School of Business"
+date: "2024-10-01"
+type: "Work in Progress"
+draft: false
+tags: ""
+summary: ""
+---
